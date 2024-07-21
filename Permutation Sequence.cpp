0c3a24356ab27e65ed37c99e986b2c47832e65b6
SUSHANTH R HEGDE <120232856+SushanthRHegde@users.noreply.github.com>
2024-07-21 22:17:31 +0530

    Create  Permutation Sequence.cpp

---
+++ Permutation Sequence.cpp
@@ -0,0 +1,27 @@
+//Using Recursion --> but time limit exceeded
+
+class Solution {
+public:
+    void solve(string s, int ind, vector<string>& res) {
+        if (ind == s.size()) {
+            res.push_back(s);
+            return;
+        }
+        for (int i = ind; i < s.size(); i++) {
+            swap(s[i], s[ind]);
+            solve(s, ind + 1, res);
+            swap(s[i], s[ind]);
+        }
+    }
+    
+    string getPermutation(int n, int k) {
+        string s;
+        vector<string> res;
+        for (int i = 1; i <= n; i++) {
+            s.push_back(i + '0');
+        }
+        solve(s, 0, res);
+        sort(res.begin(), res.end());
+        return res[k - 1];
+    }
+};
